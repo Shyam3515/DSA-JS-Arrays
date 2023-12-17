33e279940e09755e6c3f1232c1cec69c20671654
Yadla Syam Sundar <115862918+Shyam3515@users.noreply.github.com>
2023-12-17 12:36:10 +0530

    Add files via upload

--- KadanesAlgorithm.js
+++ KadanesAlgorithm.js
@@ -4,26 +4,26 @@
  */
 
 function maxSumInSubArray(){
-    let array = [1,-2,1,-3,4,2,1,3,2,-5,1,1];
+    let array = [1,-2,1,-3,4,2,1,3,1,1];
     let sum = 0, max_so_far = array[0];
     let start = 0,end = 0, nextStart = 0;
     let ele = [];//for storing sum elements => O(n)
     array.forEach((element,index) => {
         sum += element;
-        if(max_so_far < sum){
+        if(sum > max_so_far){
             max_so_far = sum;
             start = nextStart;
             end = index;
         }
-        if(sum<0){
+        if(sum <= 0){
             sum = 0;
             nextStart = index + 1;
         }
     });
-    console.log(`Start of sub-array is at ${start} index and end is at ${end}`);
-    for (let index = start; index < end; index++) {
+    console.log(`Start of sub-array is at index ${start} and end is at index ${end}`);
+    for (let index = start; index <= end; index++) {
         ele.push(array[index]); 
     }
-    console.log("The elements are :",...ele);//spread operator
+    console.log(`The elements are : ${ele} and the sum is ${max_so_far}` );
 };
 maxSumInSubArray();
